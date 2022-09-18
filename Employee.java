b1884b2624bfd60f8e1b514aae11027219405a8c
kalailincoln <112810051+kalailincoln@users.noreply.github.com>
2022-09-18 20:30:48 +0530

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,13 @@
+package javaconcepts;
+
+public class Employee {
+	public Employee(String name) {
+		System.out.println(name);
+	}
+
+	public static void main(String[] args) {
+		Employee emp = new Employee("kalaiarasan");
+		System.out.println(emp);
+	}
+
+}
